aa7a52da2801aec32d95cf70df099a4af150ceea
Martin Baillie <martin@baillie.id>
2024-12-31 22:30:00 +1100

    Remove obsolete fix-window-role patch for Emacs 30
    Signed-off-by: Martin Baillie <martin@baillie.id>

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
     { ... }:
     {
       emacs30 = [
-        ./emacs30/fix-window-role.patch
         ./emacs30/round-undecorated-frame.patch
         ./emacs30/alpha-background.patch
         ./emacs30/blur.patch
