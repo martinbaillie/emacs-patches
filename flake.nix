f26e70d1633696df5246b68adc38015af70898e9
Martin Baillie <martin@baillie.id>
2025-01-02 17:27:46 +1100

    Disable a few more patches for now
    Signed-off-by: Martin Baillie <martin@baillie.id>

--- flake.nix
+++ flake.nix
@@ -4,9 +4,9 @@
     { ... }:
     {
       emacs30 = [
-        ./emacs30/round-undecorated-frame.patch
-        ./emacs30/alpha-background.patch
-        ./emacs30/blur.patch
+        # ./emacs30/round-undecorated-frame.patch
+        # ./emacs30/alpha-background.patch
+        # ./emacs30/blur.patch
         ./emacs30/poll.patch
         ./emacs30/system-appearance.patch
       ];
